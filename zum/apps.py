b5153a930691aaf29f48b5c4cb17ca253f03b02a
Maria-cpp <vipisbd@gmail.com>
2024-06-29 15:44:42 +0500

    code upload

---
+++ zum/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ZumConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'zum'
---
+++ zum/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+#urlConfig
+
+urlpatterns = [
+
+    path('home/' , views.zum_home)
+
+]
